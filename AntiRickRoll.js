24c9e87743de08e0b1cc6027aab33ab925ba0d57
vahdam69420 <madhavgrimm@gmail.com>
2020-10-02 03:00:55 +0530

    Added support for multiple link checking

--- AntiRickRoll.js
+++ AntiRickRoll.js
@@ -1,9 +1,9 @@
 Links = document.links;
-youtubeLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-//link of the video
+var youtubeLinks =["https://www.youtube.com/watch?v=dQw4w9WgXcQ"  //link of the video
+              ,"https://www.youtube.com/watch?v=ub82Xb1C8os"];//link of another notoriously famous video
 let done = 0
 for (let i=0; i<Links.length; i++) {
-    if ( Links[i].href == youtubeLink )  {
+    if ( youtubeLinks.indexOf(Links[i].href) != -1 )  {
         if (!done) {
             alert("There's a rickroll in this webpage!");
             done = 1
